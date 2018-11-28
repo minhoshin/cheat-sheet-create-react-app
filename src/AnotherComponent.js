973a93035008515c1ce9617f1ed47c3293d93b56
minho.shin <minho.shin@gmail.com>
2018-11-28 11:56:16 +0900

    Sass, CSS Module 적용

---
+++ src/AnotherComponent.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import styles from './AnotherComponent.module.css';
+
+const AnotherComponent = () => {
+  return <div className={styles.wrapper}>What about CSS Module?</div>;
+};
+
+export default AnotherComponent;
---
+++ src/MyComponent.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './MyComponent.scss';
+
+const MyComponent = () => {
+  return (
+    <div className="MyComponent">
+      <div className="something-inside">Hello CRA v2</div>
+    </div>
+  );
+};
+
+export default MyComponent;
